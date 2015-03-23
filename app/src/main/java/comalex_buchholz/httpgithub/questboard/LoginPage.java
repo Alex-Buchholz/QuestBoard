db642ad733bab2c81450869ff8196b83be3ac435
Alex-Buchholz <alex.buchholz@ndsu.edu>
2015-03-22 23:14:23 -0500

    Added comments and reformatted code.

--- app/src/main/java/comalex_buchholz/httpgithub/questboard/LoginPage.java
+++ app/src/main/java/comalex_buchholz/httpgithub/questboard/LoginPage.java
@@ -123,18 +123,4 @@ public class LoginPage extends ActionBarActivity {
             }
         });
     }
-/*
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_login_page, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-
-        return super.onOptionsItemSelected(item);
-    }
-    */
 }
